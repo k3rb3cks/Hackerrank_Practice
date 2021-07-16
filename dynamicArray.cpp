a57fe50d75a8368f28a1e52a124231e08da61853
Abhishek Dhyade <abhishek.dhyade19@vit.edu>
2021-07-16 09:37:18 +0530

    Added Sparse Array and Dynamic Array Problem

---
+++ dynamicArray.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+int main()
+{
+    std::vector<std::vector<int>> queries(5);
+
+    for(int i = 0; i < 3 ; i++)
+    {
+        queries[i].resize(2);
+
+    }
+
+    std::cout<<queries.size();
+
+    std::vector<int> arr(4);
+
+    std::cout<<arr.at(3);
+}
